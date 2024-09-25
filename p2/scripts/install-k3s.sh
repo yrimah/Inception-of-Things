af160d8db97ebcb5203da3e33a215917e5c2487f
yrimah <yrimah@student.1337.ma>
2024-09-25 09:34:23 +0100

    starting the deployment configs of k3s

--- p2/scripts/install-k3s.sh
+++ p2/scripts/install-k3s.sh
@@ -4,3 +4,7 @@ apt-get update
 apt-get install curl -y
 
 curl -sfL https://get.k3s.io | sh -
+
+kubectl apply -f configs/config-webapp1-deployment.yaml
+# kubectl apply -f configs/config-webapp2-deployment.yaml
+# kubectl apply -f configs/config-webapp3-deployment.yaml
