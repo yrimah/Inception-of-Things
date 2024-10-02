5d26e75eeccd2facd32a5737cf942e15858b2bac
yrimah <yrimah@student.1337.ma>
2024-10-02 11:18:54 +0100

    configuration done, optimizing

--- p2/scripts/install-k3s.sh
+++ p2/scripts/install-k3s.sh
@@ -3,8 +3,16 @@
 apt-get update
 apt-get install curl -y
 
+# k3s Installation & setting node ip option for the script #
+
 curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110
 
-kubectl apply -f configs/config-webapp1-dep-serv.yaml
-kubectl apply -f configs/config-webapp2-dep-serv.yaml
-kubectl apply -f configs/config-webapp3-dep-serv.yaml
+# Deployments and Services Config #
+
+kubectl apply -f configs/config-webapp1.yaml
+kubectl apply -f configs/config-webapp2.yaml
+kubectl apply -f configs/config-webapp3.yaml
+
+# Ingresses Config #
+
+kubectl apply -f configs/config-ingress.yaml
