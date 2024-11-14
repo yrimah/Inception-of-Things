9df75648ff1dd9a3aef5e986545ab90eb324303b
yassine <rimahyassine.pro@gmail.com>
2024-11-14 16:07:54 +0100

    Re-configure p1

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,7 +1,8 @@
 rm -f /vagrant_data/node-token.txt
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s - -i 192.168.56.110
 while [ ! -e  "/var/lib/rancher/k3s/server/node-token" ]
 do
 sleep 1
 done
 cat "/var/lib/rancher/k3s/server/node-token" > /vagrant_data/node-token.txt
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
